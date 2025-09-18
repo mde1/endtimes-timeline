e297399b982a6f33307d3254cbef9056abc97092
Michelle Espinoza <michelleespinoza1@hotmail.com>
2025-09-18 03:09:59 +0000

    updated readme

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,27 +11,16 @@ st.set_page_config(
     layout='wide' # This is an emoji shortcode. Could be a URL too.
 )
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
 
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-
-
-# Add some spacing
-
-
-
+# Header
 st.header('End Times Timeline - This is a prototype under development', divider='gray')
 st.text("Use the buttons at the lower left of the timeline or click and drag to navigate to specific events. Use the navigation buttons overlayed to view events in chronological order.")
 ''
 
 ''
 ''
-
+# Using cdn to pull timeline - may migrate to more customizable version later
 components.iframe(src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=v2%3A2PACX-1vRd2YZLsZ0HODKikNXVtQjvGtq7DNcFY4vkRKOm_f2tMKRIx5hm49gdxD_NeyYPq_xJVW0zTieKqaaB&font=Default&lang=en&initial_zoom=2&scale_factor=2&width=100%25&height=900",
                   height=1200)
 

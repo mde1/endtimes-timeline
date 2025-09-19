4ab853f95d9601fa2354ceff79fc60c72fa3b6ba
Michelle Espinoza <michelleespinoza1@hotmail.com>
2025-09-19 01:33:58 +0000

    one liner

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,14 +14,13 @@ st.set_page_config(
 
 
 # Header
-st.header('End Times Timeline - This is a prototype under development', divider='gray')
-st.text("Use the buttons at the lower left of the timeline or click and drag to navigate to specific events. Use the navigation buttons overlayed to view events in chronological order.")
-st.text("This should be viewed from a desktop or tablet screen.")
-''
+st.header('End Times Timeline - This is a prototype under development', divider='violet')
+st.text("This should be viewed from a desktop or tablet screen. Use the buttons at the lower left of the timeline or click and drag to navigate to specific events or use the navigation buttons overlayed to view events in chronological order.")
 
-''
+
 ''
 # Using cdn to pull timeline - may migrate to more customizable version later
 components.iframe(src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=v2%3A2PACX-1vRd2YZLsZ0HODKikNXVtQjvGtq7DNcFY4vkRKOm_f2tMKRIx5hm49gdxD_NeyYPq_xJVW0zTieKqaaB&font=Default&lang=en&initial_zoom=2&scale_factor=2&width=100%25&height=900",
-                  height=1200)
+                  height=900)
 
+st.header('Additional Resources', divider='blue')
